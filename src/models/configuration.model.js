4600b453fbe716c02539866c27a6b4146ddbd4df
cuong_nl <luongcuong244@gmail.com>
2025-05-13 00:10:05 +0700

    update

--- src/models/configuration.model.js
+++ src/models/configuration.model.js
@@ -164,6 +164,11 @@ const configurationSchema = new schema({
         type: mongoose.Schema.Types.Boolean,
         default: false,
     },
+    // Other Settings
+    passwordReset: {
+        type: mongoose.Schema.Types.String,
+        default: null,
+    }
 },
 );
 
