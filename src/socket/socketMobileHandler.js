8f9ea8caf99e806f204b254dd51f39f0b2fde849
cuong_nl <luongcuong244@gmail.com>
2025-06-01 00:20:52 +0700

    - handle socket timeout ( device status )

--- src/socket/socketMobileHandler.js
+++ src/socket/socketMobileHandler.js
@@ -9,28 +9,6 @@ const socketMobileHandler = async (io, socket) => {
     
     socket.join(socket.data.deviceId);
 
-    socket.on("mobile:send:device_status", async (data) => {
-        const { webSocketId, deviceStatus } = data;
-        if (!webSocketId || !deviceStatus) {
-            socket.emit("web:receive:device_status", {
-                status: "error",
-                message: "Lỗi dữ liệu",
-            });
-            return;
-        }
-        console.log("webSocketId", webSocketId);
-        const webSocket = await getSocketById(io, webSocketId);
-        if (webSocket) {
-            webSocket.emit("web:receive:device_status", {
-                status: "success",
-                data: {
-                    deviceStatus: JSON.parse(deviceStatus),
-                },
-                message: "Trạng thái thiết bị đã được gửi",
-            });
-        }
-    });
-
     // Push message handler
     // Send push message to mobile
     // Get all messages for this device
